e41c824ceb2b3fd96e6e8e2a57ffdd0aefe98102
Sajjad <sajjad@pkteam.com>
2023-08-20 23:01:25 +0500

    updated

--- modules/Location/Views/frontend/blocks/list-locations/style_1.blade.php
+++ modules/Location/Views/frontend/blocks/list-locations/style_1.blade.php
@@ -6,22 +6,12 @@
             <div class="text">{{ $sub_title }}</div>
         </div>
 
-        <div class="row wow fadeInUp">
+        <div class="row wow fadeInUp location-row">
             @if(!empty($rows))
                 @foreach($rows as $key=>$row)
-                    @if($key == 0)
                         <div class="column col-lg-4 col-md-6 col-sm-12">
                             @include("Location::frontend.blocks.list-locations.loop")
                         </div>
-                    @else
-                        @if($key == 1 || $key == 3 || $key > 4)
-                            <div class="column col-lg-4 col-md-6 col-sm-12">
-                        @endif
-                        @include("Location::frontend.blocks.list-locations.loop")
-                        @if($key == 2 || $key == 4 || (count($rows) == 2 && $key == 1) || (count($rows) == 4 && $key == 3) || $key > 4)
-                            </div>
-                        @endif
-                    @endif
                 @endforeach
             @endif
         </div>
--- modules/Location/Views/frontend/blocks/list-locations/style_2.blade.php
+++ modules/Location/Views/frontend/blocks/list-locations/style_2.blade.php
@@ -12,19 +12,9 @@
         <div class="row wow fadeInUp">
             @if(!empty($rows))
                 @foreach($rows as $key => $row)
-                    @if($key == 0)
                         <div class="column col-lg-4 col-md-6 col-sm-12">
                             @include("Location::frontend.blocks.list-locations.loop")
                         </div>
-                    @else
-                        @if($key == 1 || $key == 3 || $key > 4)
-                            <div class="column col-lg-4 col-md-6 col-sm-12">
-                        @endif
-                            @include("Location::frontend.blocks.list-locations.loop")
-                        @if($key == 2 || $key == 4 || (count($rows) == 2 && $key == 1) || (count($rows) == 4 && $key == 3) || $key > 4)
-                            </div>
-                        @endif
-                    @endif
                 @endforeach
             @endif
         </div>
