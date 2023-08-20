e444cb8874f9a45f9805da083b9560c1aa38f41a
Sajjad <sajjad@pkteam.com>
2023-08-21 00:58:49 +0500

    updated

--- modules/Job/Views/frontend/layouts/blocks/job-categories/style_1.blade.php
+++ modules/Job/Views/frontend/layouts/blocks/job-categories/style_1.blade.php
@@ -6,14 +6,13 @@
             @if(!empty($sub_title))<div class="text">{{ $sub_title }}</div>@endif
         </div>
 
-        <div class="row wow fadeInUp">
+        <div class="row wow fadeInUp mx-md-n3 mx-n2">
             @if($job_categories)
                 @foreach($job_categories as $category)
                     @php $translation = $category->translateOrOrigin(app()->getLocale()); @endphp
                     <!-- Category Block -->
-                    <div class="category-block col-lg-4 col-md-6 col-6">
-                        <div class="inner-box">
-                            <a href="{{ route('job.search', ['category' => $category->id]) }}">
+                    <div class="category-block col-lg-4 col-md-6 col-6 d-flex px-md-3 px-2">
+                            <a class="inner-box w-100" href="{{ route('job.search', ['category' => $category->id]) }}">
                                 <div class="content @if(empty($category->icon)) no-icon @endif">
                                     @if($category->icon)
                                         <span class="icon d-none d-sm-flex {{ $category->icon }} mr-3"></span>
@@ -26,7 +25,6 @@
                                     </div>
                                 </div>
                             </a>
-                        </div>
                     </div>
                 @endforeach
             @endif
