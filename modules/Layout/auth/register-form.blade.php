0a68cbe10e7d96ff9b8b01dc5be6d9d7d2ad7bf0
Sajjad <sajjad@pkteam.com>
2023-08-31 00:50:10 +0500

    updated

--- modules/Layout/auth/register-form.blade.php
+++ modules/Layout/auth/register-form.blade.php
@@ -4,55 +4,57 @@
         <div class="form-group mb-3">
             <div class="btn-box row">
                 <div class="col-lg-6 col-md-12">
-                    <input class="checked register-check" type="radio" name="type" id="checkbox1" value="candidate" checked/>
+                    <input class="checked register-check position-absolute" type="radio" name="type" id="checkbox1" value="candidate" checked/>
                     <label for="checkbox1" class="theme-btn btn-style-four"><i class="la la-user"></i> {{ __("Candidate") }}</label>
                 </div>
                 <div class="col-lg-6 col-md-12">
-                    <input class="checked register-check" type="radio" name="type" id="checkbox2" value="employer"/>
+                    <input class="checked register-check position-absolute" type="radio" name="type" id="checkbox2" value="employer"/>
                     <label for="checkbox2" class="theme-btn btn-style-four"><i class="la la-briefcase"></i> {{ __("Employer") }}</label>
                 </div>
             </div>
         </div>
         
-            <div class="form-group mb-3">
+        <div class="form-row">
+            <div class="col-md-6 col-sm-6 col-12 form-group mb-3">
                 <label>First name</label>
                 <input type="text" name="first_name" placeholder="Add first name" required>
                 <span class="invalid-feedback error error-first_name"></span>
             </div>
-            <div class="form-group mb-3 pb-1">
+            <div class="col-md-6 col-sm-6 col-12 form-group mb-3 pb-1">
                 <label>Last name</label>
                 <input type="text" name="last_name" placeholder="Add last name" required>
                 <span class="invalid-feedback error error-last_name"></span>
             </div>
-        
-        <div class="company-name d-none">
+
+        <div class="col-md-6 col-sm-6 col-12 company-name d-none">
             <div class="form-group mb-3">
                 <label>Company name</label>
                 <input type="text" name="name" placeholder="Add company name" required>
                 <span class="invalid-feedback error error-company_name"></span>
             </div>
         </div>
-        <div class="form-group mb-3">
+        <div class="col-md-6 col-sm-6 col-12 form-group mb-3">
             <label>Phone Number</label>
             <input type="text" name="phone" placeholder="Add phone number" required>
             <span class="invalid-feedback error error-phone"></span>
         </div>
-        <div class="form-group mb-3">
+        <div class="col-md-6 col-sm-6 col-12 form-group mb-3">
             <label>{{__('Email address')}}</label>
             <input type="email" name="email" placeholder="{{__('Email address')}}" required>
             <span class="invalid-feedback error error-email"></span>
         </div>
 
-        <div class="form-group mb-3">
+        <div class="col-md-6 col-sm-6 col-12 form-group mb-3">
             <label>{{ __("Password") }}</label>
             <input id="password-field" type="password" name="password" value="" placeholder="{{ __("Password") }}">
             <span class="invalid-feedback error error-password"></span>
         </div>
-        <div class="form-group mb-3">
+        <div class="col-md-6 col-sm-6 col-12 form-group mb-3">
             <label>Confirm Password</label>
             <input id="confirm-password-field" type="password" name="password_confirmation" value="" placeholder="Confirm password">
             <span class="invalid-feedback error error-password_confirmation"></span>
         </div>
+    </div>
 
         @if(setting_item("recaptcha_enable"))
             <div class="form-group mb-3">
