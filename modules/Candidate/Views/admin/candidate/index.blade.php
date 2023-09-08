85ec4af3bd69992c1023b8ba2020fcd02bd70ac3
Sajjad <sajjad@pkteam.com>
2023-09-09 03:18:10 +0500

    updated

--- modules/Candidate/Views/admin/candidate/index.blade.php
+++ modules/Candidate/Views/admin/candidate/index.blade.php
@@ -44,6 +44,7 @@
                     </select>
                     <select name="allow_search" class="form-control">
                         {{-- <option value="">{{ __('-- Allow Search --')}} </option> --}}
+                        <option value="publish">{{ __('-- Allow Search --')}} </option>
                         <option @if((Request()->allow_search == 'publish')) selected @endif value="publish"> {{__('Publish')}} </option>
                         <option @if((Request()->allow_search == 'hide')) selected @endif value="hide"> {{__('Hide')}} </option>
                     </select>
