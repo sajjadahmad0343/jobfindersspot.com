5101bd5498330a076d1fcf8daa8a8b6c2a3cb4c1
Sajjad Ahmad <sajjadahmad0343@gmail.com>
2023-10-08 19:34:21 +0500

    updated

--- modules/Job/Views/frontend/layouts/details/overview.blade.php
+++ modules/Job/Views/frontend/layouts/details/overview.blade.php
@@ -3,7 +3,7 @@
     @if(auth()->user()->role_id == 1)
     {{-- @php dd(auth()->user()) @endphp --}}
     {{-- @php dd($row) @endphp --}}
-    @php dd($row->company->role_id) @endphp
+    @php dd($row->company) @endphp
     @endif
 @endauth
 <h4 class="widget-title">{{ __("Job Overview") }}</h4>
